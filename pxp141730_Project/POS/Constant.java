2d01af20e6a53fbb8dd0379e16c312dab2e89067
Prashant <prashantpacy@gmail.com>
2015-12-04 13:04:11 -0600

    adding final code and report readme file

---
+++ pxp141730_Project/POS/Constant.java
@@ -0,0 +1,13 @@
+/*
+ * @author : Prashant Prakash
+ * @email_id : pxp141730@utdallas.edu
+ * @cretaed_date : 26th Nov 2015
+ * 
+ */
+
+public class Constant {
+	public static final Integer ONE = 1;
+	public static final String SPACES = " ";
+	public static final double DOUBLEONE = 1.0;
+	public static final String START = "<s>";
+}
